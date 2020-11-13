0c7ceb7c9ac72d57377067d4acc905822585eae9
Danny Lee <72411410+dannyhlee@users.noreply.github.com>
2020-11-13 11:23:20 -0500

    Create caseClassSample.scala

---
+++ Sample_data/caseClassSample.scala
@@ -0,0 +1,21 @@
+// Possible case class structure
+
+case class Trends(
+  name: String,
+  url: String,
+  promoted_content: String,
+  query: String,
+  tweet_volume: Double
+)
+
+case class Locations(
+  name: String,
+  woeid: Double
+)
+
+case class R00tJsonObject(
+  trends: List[Trends],
+  as_of: String,
+  created_at: String,
+  locations: List[Locations]
+)
